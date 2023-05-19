5b04ea9dc9544575ca477173a387a93878cf53dd
builde7b0b <ggives3@gmail.com>
2023-05-18 20:20:00 -0700

    implement form

---
+++ daoconnect-frontend/src/components/RegistrationForm.vue
@@ -0,0 +1,32 @@
+<template>
+    <form @submit="registerUser">
+      <label for="username">Username</label>
+      <input type="text" id="username" v-model="username" required>
+  
+      <label for="email">Email</label>
+      <input type="email" id="email" v-model="email" required>
+  
+      <label for="password">Password</label>
+      <input type="password" id="password" v-model="password" required>
+  
+      <button type="submit">Register</button>
+    </form>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        username: '',
+        email: '',
+        password: ''
+      };
+    },
+    methods: {
+      registerUser() {
+        // Implement the registration logic here
+      }
+    }
+  };
+  </script>
+  
