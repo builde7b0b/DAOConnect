0adb2350028ae32cfaf6659d7a4c6e66171dbf00
builde7b0b <ggives3@gmail.com>
2023-05-18 21:03:21 -0700

    last commit of day

--- daoconnect-frontend/src/App.vue
+++ daoconnect-frontend/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <img alt="DaoConnect logo"   src="">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
   <div id="app">
     <RegistrationForm />
