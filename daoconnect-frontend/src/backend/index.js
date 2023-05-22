5463973d245877c50cf2710272dac3e6d6568a94
builde7b0b <ggives3@gmail.com>
2023-05-21 19:12:12 -0700

    init blockchain provider

--- daoconnect-frontend/src/backend/index.js
+++ daoconnect-frontend/src/backend/index.js
@@ -1,6 +1,11 @@
 import { ComposeClient } from '@composedb/client';
+import { EthereumWebAuth, getAccountId } from '@didtools/pkh-ethereum';
+import { ComposeClient } from '@composed/client'
+import {ethers } from 'ethers';
 
 
-const client = new ComposeClient({
-    
-})
+const provider = new ethers.providers.JsonRpcProvider('');
+
+
+
+const client = new ComposeClient()
