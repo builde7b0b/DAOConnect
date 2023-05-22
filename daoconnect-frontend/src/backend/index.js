78f7f201cd2ba23683f427f280387f2ee09d5455
builde7b0b <ggives3@gmail.com>
2023-05-21 19:16:14 -0700

    implement DID provider

--- daoconnect-frontend/src/backend/index.js
+++ daoconnect-frontend/src/backend/index.js
@@ -6,6 +6,21 @@ import {ethers } from 'ethers';
 
 const provider = new ethers.providers.JsonRpcProvider('');
 
+// Request Ethereum accounts from the provider 
+const addresses = await provider.send('eth_requestAccounts', []);
 
+//Get the first accounts ID
+const accountId = await getAccountId(provider, addresses[0]);
 
+//Get the Ethereum auth method 
+const authMethod = await EthereumWebAuth.getAuthMethod(provider, accountId);
+
+// create a composeClient instance
 const client = new ComposeClient()
+
+//Authorize the session using the AuthMethod 
+const session = await DIDSession.authorize(authMethod, { resources: ComposeClient.resources});
+
+// Set the DID for the Compose Client
+ComposeClient.setDID(session.did);
+
