9f24f648b45c4c49e6acf99f0fd47c7deeae9b2f
builde7b0b <ggives3@gmail.com>
2023-05-21 17:27:02 -0700

    login

---
+++ daoconnect-frontend/src/components/LoginForm.vue
@@ -0,0 +1,34 @@
+<template>
+
+    <form @submit="login"></form>
+    <div>
+        <label for="username">Username/Email</label>
+        <input type="text" id="username" v-model="username" required>
+    </div>
+    <div>
+        <label for="password">Password:</label>
+        <input type="password" id="password" v-model="password" required>
+    </div>
+    <button type="submit">Login</button>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            username: '',
+            password: ''
+        };
+    },
+    methods: {
+        login(event) {
+            event.preventDefault();
+
+            //Make API request to backend for login
+            const loginData = {
+                username: this.username,
+                password: this.password
+            }
+        }
+    }
+}</script>
