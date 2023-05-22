8590f2bfe0f0877900483ebf244272cceb0f93e5
builde7b0b <ggives3@gmail.com>
2023-05-21 17:40:30 -0700

    express

---
+++ daoconnect-frontend/src/backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+// define routes and middleware 
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`server is running on Port ${PORT}`)
+})
